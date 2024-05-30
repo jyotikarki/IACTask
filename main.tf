09f69ad4a37f92543a89b207fb302f8beaaa437f
jojo <jyotidkarki@gmail.com>
2024-05-30 12:44:20 +0530

    lass

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ provider "google" {
   region  = var.location
 }  
 
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = var.location
-  force_destroy = true
-}
+
 module "gcs" {
   source  = "./modules/gcs"
 }
