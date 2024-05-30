b804b7d8c97098a1277aa7728be481f4814a0280
jojo <jyotidkarki@gmail.com>
2024-05-30 18:22:47 +0530

    it will work  nownb

--- modules/common/variables.tf
+++ modules/common/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "vpc_name" {
   description = "The name of the VPC network."
   type        = string
-  default = "myvpc11d22"
+  default = "myvpc11dhgcfjgcjgcjgvcjvc22"
 }
 
 variable "vpc_cidr_block" {
@@ -29,7 +29,7 @@ variable "public_subnets" {
   }))
   default = [
     {
-      name = "default-public-subnet-3ef"
+      name = "default-pubnvcngvnvlic-subnet-3ef"
       cidr = "10.0.1.0/24"
     }
   ]
@@ -43,7 +43,7 @@ variable "private_subnets" {
   }))
   default = [
     {
-      name = "default-private-subnet-evd3"
+      name = "default-prmbljivate-subnet-evd3"
       cidr = "10.0.3.0/24"
     }
   ]
