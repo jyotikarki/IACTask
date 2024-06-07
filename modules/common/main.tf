a227e8c270098654f70dd00484f69bace2c63075
jojo <jyotidkarki@gmail.com>
2024-06-07 13:50:27 +0530

    ppppp

--- modules/common/main.tf
+++ modules/common/main.tf
@@ -2,7 +2,6 @@ resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
 }
-
 resource "google_compute_subnetwork" "public_subnet" {
   count                    = length(var.public_subnets)
   name                     = var.public_subnets[count.index].name
@@ -11,8 +10,6 @@ resource "google_compute_subnetwork" "public_subnet" {
   network                  = google_compute_network.vpc_network.self_link
   private_ip_google_access = true
 }
-
-
 resource "google_compute_subnetwork" "private_subnet" {
   count                    = length(var.private_subnets)
   name                     = var.private_subnets[count.index].name
