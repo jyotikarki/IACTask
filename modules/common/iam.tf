9f2a9b26ee63fa85356057ebcb48c23c4b64853a
jojo <jyotidkarki@gmail.com>
2024-06-07 14:52:59 +0530

    kmmkmk

--- modules/common/iam.tf
+++ modules/common/iam.tf
@@ -1,18 +1,3 @@
-variable "project_id" {
-  description = "The ID of the project where the resources will be created."
-  type        = string
-}
-
-variable "region" {
-  description = "The region where the resources will be created."
-  type        = string
-}
-
-variable "service_account_email" {
-  description = "The email of the service account to which the IAM roles will be assigned."
-  type        = string
-}
-
 resource "google_project_iam_binding" "viewer_role" {
   project = var.project_id
   role    = "roles/viewer"
