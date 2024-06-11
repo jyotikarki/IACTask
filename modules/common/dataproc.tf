0277d9c5fe873aaa928fb9217ce056c65cfac999
jojo <jyotidkarki@gmail.com>
2024-06-11 15:48:47 +0530

    datapropc

--- modules/common/dataproc.tf
+++ modules/common/dataproc.tf
@@ -1,4 +1,3 @@
-
 resource "google_dataproc_cluster" "cluster" {
   name       = "example-cluster"
   project    = var.project_id
@@ -6,14 +5,15 @@ resource "google_dataproc_cluster" "cluster" {
   cluster_config {
     master_config {
       num_instances = 1
-      machine_type  = "n1-standard-4"
+      machine_type  = "n1-standard-2"  # Smaller machine type
     }
     worker_config {
       num_instances = 2
-      machine_type  = "n1-standard-4"
+      machine_type  = "n1-standard-2"  # Smaller machine type
     }
   }
 }
+
 
 output "dataproc_cluster_name" {
   value = google_dataproc_cluster.cluster.name
