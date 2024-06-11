1c717ba55429ac341709b1e9e18baccbc3ebd80b
jojo <jyotidkarki@gmail.com>
2024-06-11 15:53:48 +0530

    dataproc

--- modules/common/dataproc.tf
+++ modules/common/dataproc.tf
@@ -1,18 +1,27 @@
 resource "google_dataproc_cluster" "cluster" {
-  name       = "example-cluster"
-  project    = var.project_id
-  region     = var.region
+  name   = "example-cluster"
+  project = var.project_id
+  region = var.region
+
   cluster_config {
     master_config {
       num_instances = 1
-      machine_type  = "n1-standard-2"  # Smaller machine type
+      machine_type  = "n1-standard-2"
+      disk_config {
+        boot_disk_size_gb = 50  # Adjust the size accordingly
+      }
     }
+
     worker_config {
       num_instances = 2
-      machine_type  = "n1-standard-2"  # Smaller machine type
+      machine_type  = "n1-standard-2"
+      disk_config {
+        boot_disk_size_gb = 50  # Adjust the size accordingly
+      }
     }
   }
 }
+
 
 
 output "dataproc_cluster_name" {
